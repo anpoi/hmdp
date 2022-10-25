0fde2a66d074507071a6a85fa5f15d64b5e89b22
ljd <root@qq.com>
2022-10-25 10:13:29 +0800

    完善手机验证码接口

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpSession;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
@@ -27,16 +30,18 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         if (RegexUtils.isPhoneInvalid(phone)){
             //2 如果不符合，返回错误信息
             return Result.fail("手机号格式错误！");
-
         }
 
         //3 符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
 
         //4 保存验证码到session
+        session.setAttribute("code",code);
 
         //5 发送验证码
+        log.debug("短信验证码发送成功，验证码："+code);
 
         //返回结果
-        return Result.ok();
+        return Result.ok(code);
     }
 }
