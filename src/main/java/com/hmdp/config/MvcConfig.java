4c609fd2d4e176c174dbf7d668f7b7aa2c719790
ljd <root@qq.com>
2022-10-26 11:00:10 +0800

    设置刷新拦截器

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -17,7 +18,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(redisTemplate))
+        //登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
                 .excludePathPatterns(//不用拦截的路径
                   "/user/code",
                   "/user/login",
@@ -26,6 +28,8 @@ public class MvcConfig implements WebMvcConfigurer {
                   "/shop-type/**",
                   "/voucher/**",
                   "/upload/**"
-                );
+                ).order(1);
+        //刷新拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(redisTemplate)).addPathPatterns("/**").order(0);
     }
 }
