3f8c75ebc59bea1573ba2ed4e25356aefba0bf93
ljd <root@qq.com>
2022-11-15 11:18:47 +0800

    使用sortSet存放收件信息（list和sortSet实现滚动分页）

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -8,10 +8,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IFollowService;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
@@ -27,6 +29,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
+import static com.hmdp.utils.RedisConstants.FEED_KEY;
 import static net.sf.jsqlparser.util.validation.metadata.NamedObject.user;
 
 /**
@@ -45,6 +48,9 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private IFollowService followService;
 
     @Override
     public Result queryHotBlog(Integer current) {
@@ -141,6 +147,34 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
         //4 返回
         return Result.ok(userDTOS);
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        //1 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        //2 保存探店博文
+        boolean isSuccess = save(blog);
+        if (!isSuccess) {
+            return Result.fail("新增补笔记失败！");
+        }
+
+        //3 查询笔记作者的所有粉丝 select * from tb_follow
+        List<Follow> follows = followService.query().eq("follow_user_id", user.getId()).list();
+
+        //4 推送笔记id给所有粉丝
+         for (Follow follow : follows){
+             //4.1 获取粉丝id
+             Long userId = follow.getUserId();
+             //4.2 推送到收件箱
+             String key = FEED_KEY + userId;
+             redisTemplate.opsForZSet().add(key,blog.getId().toString(),System.currentTimeMillis());
+         }
+        //
+
+        //3 返回id
+        return Result.ok(blog.getId());
     }
 
     private void queryBlogUser(Blog blog) {
